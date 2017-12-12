f20808b95015c679d3e5dccaebb8a295ca5e9db9
Dang Nguyen Anh Khoa <kdang@axon.com>
2017-12-12 18:09:08 +0700

    trying tensorboardtrying tensorboardtrying tensorboardtrying tensorboardtrying tensorboardtrying tensorboardtrying tensorboardtrying tensorboard

--- workbook/multibatch_gradient_descent.py
+++ workbook/multibatch_gradient_descent.py
@@ -2,17 +2,17 @@ import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import fetch_california_housing
 import numpy as np
+import random
 
 housing = fetch_california_housing()
 m, n = housing.data.shape
-housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
 
 scaler = StandardScaler()
 scaled_housing_data = scaler.fit_transform(housing.data)
 scaled_housing_data_plus_bias = np.c_[np.ones((m, 1)), scaled_housing_data]
 
-n_epochs = 1000
-learning_reate = 0.01
+n_epochs = 10000
+learning_rate = 0.001
 batch_size = 10
 n_batches = int(np.ceil(n / batch_size)) + 1
 
@@ -27,19 +27,31 @@ def fetch_batch(epoch, batch_index, batch_size):
 
 X = tf.placeholder(tf.float32, shape=(None, n + 1), name='X')
 y = tf.placeholder(tf.float32, shape=(None, 1), name='y')
-theta = tf.Variable(tf.random_uniform([n + 1, 1], -1, 1, seed=42), name='theta')
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1., 1., seed=42), name='theta')
 y_pred = tf.matmul(X, theta, name='predictions')
 error = y_pred - y
 mse = tf.reduce_mean(tf.square(error), name='mse')
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_reate)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
 training_op = optimizer.minimize(mse)
+tf.summary.scalar(mse)
+
 init = tf.global_variables_initializer()
+data = list(zip(scaled_housing_data_plus_bias, housing.target.reshape(-1, 1)))
 with tf.Session() as sess:
     sess.run(init)
+    train_writer = tf.summary.FileWriter('./train', sess.graph)
     for epoch in range(n_epochs):
+        total_loss = 0
+        random.shuffle(data)
         for batch_index in range(n_batches):
-            X_batch, y_batch = fetch_batch(epoch, batch_index, batch_size)
-            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+            mini_batches = data[batch_index * batch_size: (batch_index + 1) * batch_size]
+            X_batch = [i[0] for i in mini_batches]
+            y_batch = [i[1] for i in mini_batches]
+            _, loss = sess.run([training_op, mse], feed_dict={X: X_batch, y: y_batch})
+            total_loss += loss
+        train_writer.add_summary()
+        if epoch % 10 == 0:
+            print('loss at epoch {} = {}'.format(epoch, total_loss))
     best_theta = theta.eval()
 
 print('best theta')
